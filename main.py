b052eb49f9e644131d5e24811d577a8987649433
amaelane <samcarter5224@comcast.net>
2025-05-31 11:16:27 -0500

    250531 game initialization

--- main.py
+++ main.py
@@ -2,9 +2,34 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    # initialize pygame
+    pygame.init()
+
+    # welcome message
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # initialize screen constants
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # screen refresh loop
+    while True:
+        #print("looping") # debug message
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # black screen
+        screen.fill("black")
+
+        # screen refresh
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
