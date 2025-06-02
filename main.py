ba7e129edb0fa9411a77b5dce2f0b465d79988b9
amaelane <samcarter5224@comcast.net>
2025-06-02 18:19:41 -0500

    Ch3 L2: Collisions, or: Learn the difference between classes and instances, you mook

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -45,6 +46,12 @@ def main():
 
         # update updatables
         updatable.update(dt)
+
+        # collision check
+        for asteroid in asteroids:
+            if asteroid.collision(player) == True:
+                print("Game over!")
+                sys.exit()
 
         # black screen
         screen.fill("black")
