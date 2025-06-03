94c01941555fbc3b7211edf0a9ff08ba046008e5
amaelane <samcarter5224@comcast.net>
2025-06-03 15:26:25 -0500

    250603 Ch3 L4: Rate Limit - My Code

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
 
     # in the player class
     def triangle(self):
@@ -40,8 +41,13 @@ class Player(CircleShape):
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.cooldown > 0:
+                self.cooldown -= dt
+            else:
+                self.shoot()
+                self.cooldown -= dt
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.cooldown += PLAYER_SHOOT_COOLDOWN
